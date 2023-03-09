367a4c18c4808f1f98edf89d186268f95c1cbbb6
Priscila <priscilasartori2@gmail.com>
2023-03-08 22:05:37 -0300

    feat: Requisito 07 - Crie o modelo Category em src/models/Category.js com as propriedades corretas

---
+++ src/models/Category.js
@@ -0,0 +1,15 @@
+const CategorySchema = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true },
+    name: DataTypes.STRING,
+  },
+  {
+    tableName: 'categories',
+    underscored: true, // snake_case e camelCase
+    timestamps: false, // Não gera a createdAt e updatedAt (data de criação e ultima atualização, em migrations)
+  });
+  return UserTable;
+};
+
+
+module.exports = CategorySchema;
